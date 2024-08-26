e9bfe6de09ce2d18dce9a2cac0cba3dd6542c029
i-nap <Inap.S022@virinchicollege.edu.np>
2024-08-26 19:32:33 +0545

    try

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <h1>hora</h1>
       <Image src="/image.png" alt="image" width={500} height={500} />
     </div>
   );
